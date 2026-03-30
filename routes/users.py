699408c9252cf3379067762f79388b141fcab810
Immxdium <Immxdium@github.com>
2026-03-30 13:49:16 -0500

    modified:   routes/users.py

--- routes/users.py
+++ routes/users.py
@@ -9,11 +9,17 @@ def get_user(lamar_id):
     response.headers['Content-Type'] = 'text/html'
     return f"User: {lamar_id}"
 
+# Soft delete - deactivates the user
 @users_bp.route('/<string:lamar_id>', methods=['PATCH'])
 def deactivate_user(lamar_id):
-    return f"User: {lamar_id} deactivated"
+    return f"User {lamar_id} has been deactivated."
 
+# Reactivate a soft deleted user
+@users_bp.route('/<string:lamar_id>/reactivate', methods=['PATCH'])
+def reactivate_user(lamar_id):
+    return f"User {lamar_id} has been reactivated."
 
-@users_bp.route('/string:lamar_id>', methods=['DELETE'])
+# Hard delete - admin only, permanently removes user
+@users_bp.route('/<string:lamar_id>', methods=['DELETE'])
 def delete_user(lamar_id):
-    return f"User: {lamar_id} deleted"
+    return f"User {lamar_id} has been permanently deleted."
