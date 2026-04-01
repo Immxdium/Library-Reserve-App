d9560ad76418e65aa021cef0f5311064c8d3ce72
Immxdium <Immxdium@github.com>
2026-04-01 12:28:24 -0500

    modified:   routes/rooms.py

--- routes/rooms.py
+++ routes/rooms.py
@@ -2,9 +2,10 @@ from flask import Flask, Blueprint, make_response
 
 rooms_bp = Blueprint('rooms', __name__)
 
-@rooms_bp.route('/<string:room_number>/availablity', methods=['GET', 'POST'])
-def reserve_room(lamar_id, room_number):
-    response = make_response()
-    response.status_code = 200
-    response.headers['Content-Type'] = 'text/html'
-    return f"Reserving room {room_number} for user {lamar_id}."
+@rooms_bp.route('/<string:room_number>/availability', methods=['GET'])
+def check_availability(room_number):
+    return f"Checking availability for room {room_number}."
+
+@rooms_bp.route('/<string:room_number>/book', methods=['POST'])
+def book_room(room_number):
+    return f"Booking room {room_number}."
